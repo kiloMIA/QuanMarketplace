96001e895e49f2e09ce669a6e16fda9cd1a2c683
kiloMIA <pipianino@protonmail.com>
2024-03-11 12:20:43 +0500

    starting the work on the repository

--- backend/internal/repository/postgres/user.go
+++ backend/internal/repository/postgres/user.go
@@ -1,8 +1,14 @@
 package postgres
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kiloMIA/QuanMarketplace/backend/internal/models"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepository struct {
@@ -15,4 +21,25 @@ func NewUserRepository(db *pgxpool.Pool, lg *logrus.Logger) *UserRepository {
 		DB: db,
 		lg: lg,
 	}
+}
+
+func (ur *UserRepository) CreateUser (ctx context.Context, user *models.User) error {
+	ur.lg.Debug("user repository level - CreateUser")
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.DefaultCost)
+	if err != nil {
+		ur.lg.Errorf("user repository level - GenerateFromPassword - error hashing plaintext password - %v", err)
+		return err
+	}
+
+	query := "INSERT INTO users (name, email, password, enable) VALUES ($1, $2, $3, $4)"
+	_, err = ur.DB.Exec(ctx, query, user.Name, user.Email, string(hashedPassword), user.Enable)
+	if err != nil {
+		if strings.Contains(err.Error(), "unique constraint") && strings.Contains(err.Error(), "Email") {
+			return fmt.Errorf("email %s already exists", user.Email)
+		}
+		ur.lg.Errorf("user repository level - CreateUser - db exec - %v", err)
+		return err
+	}
+
+	return nil
 }
--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -1,1 +1,15 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/kiloMIA/QuanMarketplace/backend/internal/models"
+)
+
+type Repository struct {
+	User
+}
+
+type User interface {
+	CreateUser (ctx context.Context, user *models.User) error
+}
